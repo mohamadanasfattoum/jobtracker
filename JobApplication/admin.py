06f913a74abda6dc62066031f4813219acd330b9
Mohamad anas fattoum <samaanas30@gmail.com>
2026-07-09 23:13:03 +0200

    feat: register job application in admin

--- JobApplication/admin.py
+++ JobApplication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import JobApplication
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ['company_name','job_title','status','application_date','created_at']
+    search_fields = ['company_name','job_title','status']
+    list_filter = ['status','created_at','application_date','source']
+
+admin.site.register(JobApplication,JobApplicationAdmin)
