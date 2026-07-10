6c2e91739b27259483ef06d724c6a2f69220a3db
Mohamad anas fattoum <samaanas30@gmail.com>
2026-07-10 19:34:44 +0200

    feat: set default application status

--- JobApplication/models.py
+++ JobApplication/models.py
@@ -16,7 +16,7 @@ class JobApplication(models.Model):
     job_url = models.URLField(blank=True)
     source = models.CharField(max_length=100, blank=True)
     application_date = models.DateField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=status_werte)
+    status = models.CharField(max_length=20, choices=status_werte,default='planned' )
     notes = models.TextField(max_length=500, blank=True)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
