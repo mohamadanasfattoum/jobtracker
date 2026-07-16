7c0294366c24aa94a762effa3080fb648b468c83
Mohamad anas fattoum <samaanas30@gmail.com>
2026-07-16 23:14:31 +0200

    feat: add job application create view

--- JobApplication/urls.py
+++ JobApplication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import JobApplicationListView
+from .views import JobApplicationListView, JobApplicationCreateView
 
 urlpatterns = [
     path('', JobApplicationListView.as_view(), name="application_list"),
+    path('create/', JobApplicationCreateView.as_view(), name="application_create"),
 ]
--- JobApplication/views.py
+++ JobApplication/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from .models import JobApplication
 from django.views import generic
+from django.urls import reverse_lazy
+
+from .models import JobApplication
+
+
 
 
 class JobApplicationListView(generic.ListView):
     model = JobApplication
     context_object_name = "applications"
+
+
+class JobApplicationCreateView(generic.CreateView):
+    model = JobApplication
+    fields = '__all__'
+    success_url = reverse_lazy('application_list')
